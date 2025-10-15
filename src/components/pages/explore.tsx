9f25087b36afd451b3172c88c5e00ad36dfd9fa2
Paulo Cunha <pc@pmrcunha.com>
2025-10-16 00:32:38 +0200

    add an empty description

--- src/components/pages/explore.tsx
+++ src/components/pages/explore.tsx
@@ -16,31 +16,77 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Button } from '../ui/button'
-import { Code, Eye } from 'lucide-react'
+import { Code, Cog, Eye, FolderGitIcon } from 'lucide-react'
 import type { Event } from '@/lib/nostr/types'
+import { ArrowUpRightIcon } from "lucide-react"
+import {
+    Empty,
+    EmptyContent,
+    EmptyDescription,
+    EmptyHeader,
+    EmptyMedia,
+    EmptyTitle,
+} from "@/components/ui/empty"
+
+function EmptyDemo() {
+    return (
+        <Empty>
+            <EmptyHeader>
+                <EmptyMedia variant="icon">
+                    <FolderGitIcon />
+                </EmptyMedia>
+                <EmptyTitle>No Events Yet</EmptyTitle>
+                <EmptyDescription>
+                    You haven&apos;t created any projects yet. Get started by creating
+                    your first project.
+                </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent>
+                <div className="flex gap-2">
+                    <Button>Create Query</Button>
+                    <Button variant="outline">Import Events</Button>
+                </div>
+            </EmptyContent>
+            <Button
+                variant="link"
+                asChild
+                className="text-muted-foreground"
+                size="sm"
+            >
+                <a href="#">
+                    Learn More <ArrowUpRightIcon />
+                </a>
+            </Button>
+        </Empty>
+    )
+}
+
 
 function LeftPanel() {
-    return <div>
-        <h2>Nostr Explorer</h2>
-        <h3>Kinds</h3>
-        <ul>
-            <li className="flex items-center gap-3">
-                <Checkbox id="kind1" />
-                <Label htmlFor="kind1">#1 Note</Label>
-            </li>
-            <li className="flex items-center gap-3">
-                <Checkbox id="kind3" />
-                <Label htmlFor="kind3">#3 Follows</Label>
-            </li>
-            <li className="flex items-center gap-3">
-                <Checkbox id="kind5" />
-                <Label htmlFor="kind5">#5 Delete Requests</Label>
-            </li>
-            <li className="flex items-center gap-3">
-                <Checkbox id="kind7" />
-                <Label htmlFor="kind7">#7 Reactions</Label>
-            </li>
-        </ul>
+    return <div className="flex flex-col justify-between h-full">
+        <div>
+            <h2>Nostr Explorer</h2>
+            <h3>Kinds</h3>
+            <ul>
+                <li className="flex items-center gap-3">
+                    <Checkbox id="kind1" />
+                    <Label htmlFor="kind1">#1 Note</Label>
+                </li>
+                <li className="flex items-center gap-3">
+                    <Checkbox id="kind3" />
+                    <Label htmlFor="kind3">#3 Follows</Label>
+                </li>
+                <li className="flex items-center gap-3">
+                    <Checkbox id="kind5" />
+                    <Label htmlFor="kind5">#5 Delete Requests</Label>
+                </li>
+                <li className="flex items-center gap-3">
+                    <Checkbox id="kind7" />
+                    <Label htmlFor="kind7">#7 Reactions</Label>
+                </li>
+            </ul>
+        </div>
+        <Button variant="outline"><Cog /> Settings</Button>
     </div>
 }
 
@@ -63,14 +109,14 @@ type MainProps = {
 }
 
 function Main({ onSearch, results }: MainProps) {
-    const [view, setView] = useState<'raw' | 'preview'>('raw')
-    return <div className="h-[85vh] overflow-y-scroll">
+    const [view, setView] = useState<'raw' | 'preview'>('preview')
+    return <div className="h-[90vh] overflow-y-scroll">
         <div>
             <SearchInput onSearch={onSearch} />
             <SortSelect />
         </div>
         <div className="flex flex-col gap-3 p-3">
-            {results.map((result, i) => {
+            {results.length > 0 ? results.map((result, i) => {
                 return <Card key={i} className="w-full">
                     <CardHeader>
                         <CardTitle>Kind 1</CardTitle>
@@ -84,7 +130,7 @@ function Main({ onSearch, results }: MainProps) {
                         {view === 'raw' ? <pre className="text-wrap overflow-x-scroll">{JSON.stringify(result, null, 2)}</pre> : <div>{result.content}</div>}
                     </CardContent>
                 </Card>
-            })}
+            }) : <div><EmptyDemo /></div>}
         </div>
     </div>
 }
@@ -131,9 +177,7 @@ export function ExplorePage() {
         className="rounded-lg border md:min-w-[450px]"
     >
         <ResizablePanel defaultSize={20}>
-            <div className="p-6">
-                <span className="font-semibold"><LeftPanel /></span>
-            </div>
+            <LeftPanel />
         </ResizablePanel>
         <ResizableHandle />
         <ResizablePanel defaultSize={60}>
