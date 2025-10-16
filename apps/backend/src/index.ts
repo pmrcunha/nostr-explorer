d65b468278fb9282d805d5cc555c3532cdf0eae9
Paulo Cunha <pc@pmrcunha.com>
2025-10-16 15:16:14 +0200

    add openapi docs and cron job sample to hono

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,33 @@
-import { Hono } from "hono";
 import queries from "./app/queries";
 import relays from "./app/relays";
+import { Cron, scheduledJobs } from "croner";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
 
-const app = new Hono();
+const app = new OpenAPIHono();
+
+const job = new Cron(
+  "*/5 * * * * *",
+  { name: "five-seconds-test", timezone: "Europe/Copenhagen" },
+  () => {
+    console.log("5 seconds have passed");
+  },
+);
+console.log(
+  `Cron job ${job.name}: ${job.isRunning() ? "running" : "not running"}`,
+);
+
+console.log(`Registered Cron jobs: ${scheduledJobs.map((job) => job.name)}`);
+
+app.doc("/doc", {
+  openapi: "3.1.0",
+  info: {
+    version: "1.0.0",
+    title: "My API",
+  },
+});
+
+app.get("/scalar", Scalar({ url: "/doc" }));
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
