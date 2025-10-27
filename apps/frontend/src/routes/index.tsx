6c60a7b0bbe53ec37d24cb7e24e4edb8648bc427
Paulo Cunha <paulomrcunha@gmail.com>
2025-10-27 22:05:32 +0100

    move explore page to routes folder

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,6 +1,33 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { ExplorePage } from "@/components/pages/explore";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { KindsPanel } from '@/components/features/kinds-panel/kinds-panel'
+import { FiltersPanel } from '@/components/features/filters-panel/filters-panel'
+import { SearchPanel } from "@/components/features/search-panel/search-panel";
 
 export const Route = createFileRoute("/")({
   component: ExplorePage,
 });
+
+export function ExplorePage() {
+  return (
+    <div className="grid grid-cols-[300px_1fr] h-full">
+      <KindsPanel />
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="md:min-w-[450px]"
+      >
+        <ResizablePanel defaultSize={60}>
+          <SearchPanel />
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={40}>
+          <FiltersPanel />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+}
